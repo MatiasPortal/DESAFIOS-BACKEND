30f4ce73310b11363739e465b082bb0a46bf8111
Matias Portal <portalmatias4@gmail.com>
2023-08-13 00:18:01 -0300

    desafio10-clase39-DocumentarApi

---
+++ desafio10-clase39/src/dao/models/users.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const collection = "users";
+
+const schema = new mongoose.Schema({
+    firstName: { type: String, trim: true },
+    lastName: { type: String, trim: true },
+    email: { type: String, trim: true },
+    age: { type: Number },
+    password: { type: String },
+    cart: { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "carts"
+    },
+    role: { type: String, trim: true, default: "user", enum: ["user", "admin", "premium"]  }
+});
+
+const userModel = mongoose.model(collection, schema);
+
+export default userModel;
